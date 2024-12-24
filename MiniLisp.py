24fb31368547e390b2e958d4ae1c2c2dfdd83d12
GeorgeShiue <georgeshiue1130@gmail.com>
2024-12-24 22:56:47 +0800

    add MiniLisp.py add grammar.lark update README.md

---
+++ MiniLisp.py
@@ -0,0 +1,302 @@
+from lark import Lark
+import os
+
+mini_lisp_grammar = ""
+with open("grammar.lark", 'r', encoding='utf-8') as f:
+    mini_lisp_grammar = f.read()
+
+# print(mini_lisp_grammar)
+
+mini_lisp_parser = Lark(mini_lisp_grammar, start='program')
+
+def type_check(parameter, type_):
+    if type_ == "int":
+        # print("int: ", isinstance(parameter, int))
+        return type(parameter) == int
+    elif type_ == "bool":
+        # print("bool: ", isinstance(parameter, bool))
+        return type(parameter) == bool
+    # elif type == "fun":
+    #     return isinstance(parameter, Lark)
+    return False
+
+global_env = {}
+
+def eval_ast(tree, param=None, local_env=None, fun_ids=False, def_fun=False):
+    # if local_env:
+    #     print(tree.data + " local_env: " + str(local_env))
+    if tree.data == 'program':
+        # print(tree.children)
+        for child in tree.children:
+            eval_ast(child)
+    elif tree.data == 'def_stmt':
+        # print(tree.children)
+        var = eval_ast(tree.children[0])
+        val = eval_ast(tree.children[1], def_fun=True)
+        global_env[var] = val
+        # print("def_stmt var: " + var)
+        # print("def_stmt val: " + str(val))
+    elif tree.data == 'variable':
+        # print(tree.children)
+        return eval_ast(tree.children[0], local_env=local_env)
+    elif tree.data == 'print_num':
+        # print(tree.children)
+        val = eval_ast(tree.children[0])
+        print("print_num: " + str(val))
+    elif tree.data == 'print_bool':
+        # print(tree.children)
+        val = eval_ast(tree.children[0])
+        if val:
+            print("#t")
+        else:
+            print("#f")
+    elif tree.data == 'plus':
+        # print(tree.children)
+        # result = sum([eval_ast(child, local_env=local_env) for child in tree.children])
+        result = 0
+        for child in tree.children:
+            num = eval_ast(child, local_env=local_env)
+            if not type_check(num, "int"):
+                raise Exception("Type Error")
+            result += num
+        # print("plus: " + str(result))
+        return result
+    elif tree.data == 'minus':
+        # print(tree.children)
+        # result = eval_ast(tree.children[0], local_env=local_env) - eval_ast(tree.children[1], local_env=local_env)
+        num1 = eval_ast(tree.children[0], local_env=local_env)
+        num2 = eval_ast(tree.children[1], local_env=local_env)
+        if not type_check(num1, "int") or not type_check(num2, "int"):
+            raise Exception("Type Error")
+        result = num1 - num2
+        # print("minus: " + str(result))
+        return result
+    elif tree.data == 'multiply':
+        # print(tree.children)
+        result = 1
+        for child in tree.children:
+            num = eval_ast(child, local_env=local_env)
+            if not type_check(num, "int"):
+                raise Exception("Type Error")
+            result *= num
+        # print("multiply: " + str(result))
+        return result
+    elif tree.data == 'divide':
+        # print(tree.children)
+        # result = int(eval_ast(tree.children[0], local_env=local_env) / eval_ast(tree.children[1], local_env=local_env))
+        num1 = eval_ast(tree.children[0], local_env=local_env)
+        num2 = eval_ast(tree.children[1], local_env=local_env)
+        if not type_check(num1, "int") or not type_check(num2, "int"):
+            raise Exception("Type Error")
+        result = int(num1 / num2)
+        # print("divide: " + str(result))
+        return result
+    elif tree.data == 'modulus':
+        # print(tree.children)
+        # result = eval_ast(tree.children[0], local_env=local_env) % eval_ast(tree.children[1], local_env=local_env)
+        num1 = eval_ast(tree.children[0], local_env=local_env)
+        num2 = eval_ast(tree.children[1], local_env=local_env)
+        if not type_check(num1, "int") or not type_check(num2, "int"):
+            raise Exception("Type Error")
+        result = num1 % num2
+        # print("modulus: " + str(result))
+        return result
+    elif tree.data == 'greater':
+        # print(tree.children)
+        # result = eval_ast(tree.children[0], local_env=local_env) > eval_ast(tree.children[1], local_env=local_env)
+        num1 = eval_ast(tree.children[0], local_env=local_env)
+        num2 = eval_ast(tree.children[1], local_env=local_env)
+        if not type_check(num1, "int") or not type_check(num2, "int"):
+            raise Exception("Type Error")
+        result = num1 > num2
+        # print("greater: " + str(result))
+        return result
+    elif tree.data == 'smaller':
+        # print(tree.children)
+        # result = eval_ast(tree.children[0], local_env=local_env) < eval_ast(tree.children[1], local_env=local_env)
+        num1 = eval_ast(tree.children[0], local_env=local_env)
+        num2 = eval_ast(tree.children[1], local_env=local_env)
+        if not type_check(num1, "int") or not type_check(num2, "int"):
+            raise Exception("Type Error")
+        result = num1 < num2
+        # print("smaller: " + str(result))
+        return result
+    elif tree.data == 'equal':
+        # print(tree.children)
+        first_num = eval_ast(tree.children[0], local_env=local_env)
+        if not type_check(first_num, "int"):
+            raise Exception("Type Error")
+        for i in range(1, len(tree.children)):
+            cur_num = eval_ast(tree.children[i], local_env=local_env)
+            if not type_check(cur_num, "int"):
+                raise Exception("Type Error")
+            if first_num != cur_num:
+                # print("equal: False")
+                return False
+        # print("equal: True")
+        return True
+    elif tree.data == 'and_op':
+        # print(tree.children)
+        # result = all([eval_ast(child, local_env=local_env) for child in tree.children])
+        result = True
+        for child in tree.children:
+            bool = eval_ast(child, local_env=local_env)
+            if not type_check(bool, "bool"):
+                raise Exception("Type Error")
+            if not bool:
+                result = False
+                break
+        # print("and: " + str(result))
+        return result
+    elif tree.data == 'or_op':
+        # print(tree.children)
+        # result = any([eval_ast(child, local_env=local_env) for child in tree.children])
+        result = False
+        for child in tree.children:
+            bool = eval_ast(child, local_env=local_env)
+            if not type_check(bool, "bool"):
+                raise Exception("Type Error")
+            if bool:
+                result = True
+                break
+        # print("or: " + str(result))
+        return result
+    elif tree.data == 'not_op':
+        # print(tree.children)
+        # result = not eval_ast(tree.children[0], local_env=local_env)
+        bool = eval_ast(tree.children[0], local_env=local_env)
+        if not type_check(bool, "bool"):
+            raise Exception("Type Error")
+        result = not bool
+        # print("not: " + str(result))
+        return result
+    elif tree.data == 'if_exp':
+        # print(tree.children)
+        test = eval_ast(tree.children[0], local_env=local_env)
+        if not type_check(test, "bool"):
+            raise Exception("Type Error")
+        # print("test result: " + str(test))
+        if test:
+            then = eval_ast(tree.children[1], local_env=local_env)
+            # print("then: " + str(then))
+            return then
+        else:
+            els = eval_ast(tree.children[2], local_env=local_env)
+            # print("else: " + str(els))
+            return els
+    elif tree.data == 'test_exp':
+        # print(tree.children)
+        result = eval_ast(tree.children[0], local_env=local_env)
+        # print("test: " + str(result))
+        return result
+    elif tree.data == 'than_exp':
+        # print(tree.children)
+        result = eval_ast(tree.children[0], local_env=local_env)
+        # print("than: " + str(result))
+        return result
+    elif tree.data == 'else_exp':
+        # print(tree.children)
+        result = eval_ast(tree.children[0], local_env=local_env)
+        # print("else: " + str(result))
+        return result
+    elif tree.data == 'fun_call':
+        # print(tree.children)
+        param = []
+        for i in range(1, len(tree.children)):
+            # print(tree.children[i])
+            param.append(eval_ast(tree.children[i], local_env=local_env))
+        # print("parm:", param)
+        # print(param)
+        result = eval_ast(tree.children[0], param=param)
+        # print("fun_call: " + str(result))
+        return result
+    elif tree.data == 'param':
+        # print(tree.children)
+        return eval_ast(tree.children[0], local_env=local_env)
+    elif tree.data == 'fun_exp':
+        # print(tree.children)
+        if def_fun:
+            return tree
+        
+        local_vars = eval_ast(tree.children[0])
+        # print("local_vars: " + str(local_vars))
+        if local_vars: # 帶有區域變數的函式
+            local_env = dict(zip(local_vars, param))
+            # print("local_env: " + str(local_env))
+            result = eval_ast(tree.children[1], local_env=local_env)
+            # print("fun_exp: " + str(result))
+            return result
+        else: # 無區域變數的函式
+            result = eval_ast(tree.children[1])
+            # print("fun_exp: " + str(result))
+            return result
+    elif tree.data == 'fun_ids':
+        # print(tree.children)
+        local_vars = []
+        for child in tree.children:
+            local_vars.append(eval_ast(child, fun_ids=True))
+        # print("fun_ids: " + str(local_vars))
+        return local_vars
+    elif tree.data == 'fun_body':
+        # print(tree.children)
+        result = eval_ast(tree.children[0], local_env=local_env)
+        # print("fun_body: " + str(result))
+        return result
+    elif tree.data == 'fun_name':
+        fun_var = eval_ast(tree.children[0])
+        # print("fun_var:", fun_var)
+        # print("param:", param)
+        result = eval_ast(fun_var, param=param)
+        # print("fun_name: " + str(result))
+        return result
+    elif tree.data == 'id':
+        # print(tree.children)
+        # print("id: " + tree.children[0])
+        if local_env and tree.children[0] in local_env:
+            return local_env[tree.children[0]]
+        if not fun_ids and tree.children[0] in global_env:
+            return global_env[tree.children[0]]
+        return tree.children[0]
+    elif tree.data == 'number':
+        # print(tree.children)
+        # print("number: " + tree.children[0])
+        return int(tree.children[0])
+    elif tree.data == 'bool_val':
+        # print(tree.children)
+        if tree.children[0] == "#t":
+            return True
+        return False
+    
+folder_path = './public_test_data'
+
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        # print(os.path.join(root, file))
+        code = ""
+        # file_path = "./public_test_data/b2_2.lsp"
+        file_path = os.path.join(root, file)
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                code = file.read()
+                # print("檔案內容:")
+                # print(content)
+        except FileNotFoundError:
+            print(f"檔案 {file_path} 不存在。")
+        except Exception as e:
+            print(f"讀取檔案時發生錯誤: {e}")
+
+        # code = """
+        # """
+
+        print(file_path)
+        try:
+            tree = mini_lisp_parser.parse(code)
+            # print(tree.pretty())
+            eval_ast(tree)
+        except Exception as e:
+            if str(e) == "Type Error":
+                print("Type Error")
+            else:
+                # print("syntax error: ", e)
+                print("syntax error")
+        print()
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Lark
+# MiniLisp Interpreter
+
+此**Final Project**是一個 **MiniLisp Interpreter**，使用 Python 實作，並運用 **Lark** 套件處理 MiniLisp 的語法與執行解析後的抽象語法樹 (AST)。
+
+---
+
+## 功能
+
+- **基本算術運算**：支援加法、減法、乘法、除法與取餘。
+- **布林運算**：處理邏輯運算符，例如 `and`、`or` 和 `not`。
+- **關係運算**：包含比較運算符，例如 `greater`、`smaller` 和 `equal`。
+- **條件語句**：實作 `if-else` 表達式，用於決策。
+- **變數定義**：允許在表達式中宣告與使用變數。
+- **函數定義與呼叫**：支援用戶自定義函數，包含參數與區域變數。
+- **輸入解析**：可從 `.lsp` 檔案中讀取並執行 MiniLisp 程式。
+- **錯誤處理**：包含強大的型別檢查與語法錯誤偵測。
+
+---
+
+## 專案結構
+
+### 主要檔案
+
+- **`MiniLisp.py`**：主要的直譯器，用於解析並執行 MiniLisp 程式。
+- **`grammar.lark`**：定義 MiniLisp 語法規則的 Lark 文法檔案。
+- **`public_test_data/`**：包含範例 `.lsp` 檔案的資料夾，用於測試直譯器。
+
+---
+
+## Quick Start
+
+### 環境需求
+
+- **Python 3.8+**
+- **Lark Library**：透過 pip 安裝：
+  ```bash
+  pip install lark
